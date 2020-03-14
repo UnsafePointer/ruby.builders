ac27960fe793d4fa0465bddd2bc6c7c3b4798d41
Renzo Crisostomo <renzo@crisostomo.me>
2020-03-14 09:23:52 +0100

    Create public subnets

--- main.tf
+++ main.tf
@@ -24,3 +24,14 @@ resource "aws_subnet" "private" {
     Name = "${local.vpc_name}_private_${local.availability_zones[count.index]}"
   }
 }
+
+resource "aws_subnet" "public" {
+  count = length(local.availability_zones)
+  cidr_block = cidrsubnet(aws_vpc.buildbot_micro.cidr_block, 3, length(local.availability_zones) + count.index)
+  availability_zone = local.availability_zones[count.index]
+  vpc_id = aws_vpc.buildbot_micro.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.vpc_name}_public_${local.availability_zones[count.index]}"
+  }
+}
