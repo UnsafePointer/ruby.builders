2ae2c3bf67f72d95e1258a6d2a2f8c3a12395d75
Renzo Crisostomo <renzo@crisostomo.me>
2020-03-14 09:17:59 +0100

    Create private subnets

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ provider "aws" {
 }
 
 locals {
+  vpc_name = "buildbot_micro"
   availability_zones = ["us-east-1a", "us-east-1b"]
 }
 
 resource "aws_vpc" "buildbot_micro" {
   cidr_block = "172.18.0.0/24"
   tags = {
-    Name = "buildbot_micro"
+    Name = local.vpc_name
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(local.availability_zones)
+  cidr_block = cidrsubnet(aws_vpc.buildbot_micro.cidr_block, 3, count.index)
+  availability_zone = local.availability_zones[count.index]
+  vpc_id = aws_vpc.buildbot_micro.id
+  tags = {
+    Name = "${local.vpc_name}_private_${local.availability_zones[count.index]}"
   }
 }
