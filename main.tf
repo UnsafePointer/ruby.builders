338b2d86e81288e820fb49c39cf4282c5c8bb0ee
Renzo Crisostomo <renzo@crisostomo.me>
2020-03-14 15:47:42 +0100

    Create IAM ECS tasks policy

--- main.tf
+++ main.tf
@@ -192,3 +192,25 @@ resource "aws_alb_listener" "buildbot_listener" {
     type = "forward"
   }
 }
+
+# IAM ECS tasks policy
+
+data "aws_iam_policy_document" "ecs_tasks_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_tasks_execution_role" {
+  name = "${local.vpc_name}_ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_tasks_policy_attachment" {
+  role = aws_iam_role.ecs_tasks_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
