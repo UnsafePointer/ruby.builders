33a82ad55562606edee0dfb4f0eda4da2e6e3c0c
Renzo Crisostomo <renzo@crisostomo.me>
2020-03-17 18:17:49 +0100

    Create IAM role for EC2 instance wokers to access SSM service

--- main.tf
+++ main.tf
@@ -360,6 +360,57 @@ resource "aws_iam_role_policy_attachment" "ecs_tasks_ssm_policy_attachment" {
   policy_arn = aws_iam_policy.aws_iam_policy_buildbot_ssm.arn
 }
 
+data "aws_iam_policy_document" "ec2_instances_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "aws_iam_policy_buildbot_worker_ssm" {
+  name        = "aws_iam_policy_buildbot_worker_ssm"
+  path        = "/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeParameters"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameter"
+            ],
+            "Resource": "arn:aws:ssm:${local.region}:${data.aws_caller_identity.current.account_id}:parameter/buildbot/worker/*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ec2_buildbot_worker_role" {
+  name = "${local.vpc_name}_ec2_buildbot_worker_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_instances_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "buildbot_worker_ssm_policy_attachment" {
+  role = aws_iam_role.ec2_buildbot_worker_role.name
+  policy_arn = aws_iam_policy.aws_iam_policy_buildbot_worker_ssm.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_buildbot_worker_instance_profile" {
+  name = "ec2_buildbot_worker_instance_profile"
+  role = "${aws_iam_role.ec2_buildbot_worker_role.name}"
+}
+
 # ECS task with Fargate launch type
 
 resource "aws_ecs_cluster" "ecs_cluster" {
