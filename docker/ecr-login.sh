902a3d6ed3365a2f539709d72625626815cd36b7
Renzo Crisostomo <renzo@crisostomo.me>
2020-05-02 15:42:21 +0200

    Remove Docker resources

--- docker/ecr-login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-
-aws ecr get-login-password \
-    --region $REGION \
-| docker login \
-    --username AWS \
-    --password-stdin "${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com"
